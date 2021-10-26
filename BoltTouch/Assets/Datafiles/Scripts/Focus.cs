4af0658e354116f619eeb5d5adc26f99f32e0eb0
Larissa Negris de Souza <larissa.negris.souza@gmail.com>
2021-10-26 16:33:05 -0300

    Adicao arquivos iniciais
    Arquivos iniciais unity

---
+++ BoltTouch/Assets/Datafiles/Scripts/Focus.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using Vuforia;
+
+public class Focus : MonoBehaviour
+{
+    void Start()
+    {
+        var vuforia = VuforiaARController.Instance;
+        vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        vuforia.RegisterOnPauseCallback(OnPaused);
+    }
+
+    private void OnVuforiaStarted()
+    {
+        CameraDevice.Instance.SetFocusMode(
+            CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+    private void OnPaused(bool paused)
+    {
+        if (!paused) // resumed
+        {
+            // Set again autofocus mode when app is resumed
+            CameraDevice.Instance.SetFocusMode(
+                CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+    }
+}
