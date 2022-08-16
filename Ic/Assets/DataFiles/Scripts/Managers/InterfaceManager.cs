95e46e6f72e3409f0513d6ece47ef2feab3dde28
Lucca Paiva <luccafpaiva@gmail.com>
2022-08-16 14:22:33 -0300

    implementacao botoes mudanca cor
    implementacao do codigo que faz com que os botoes de baixo mudem a cor do objeto selecionado

--- Ic/Assets/DataFiles/Scripts/Managers/InterfaceManager.cs
+++ Ic/Assets/DataFiles/Scripts/Managers/InterfaceManager.cs
@@ -5,20 +5,43 @@ using UnityEngine.UI;
 
 public class InterfaceManager : MonoBehaviour
 {
+    public GameObject parent;
+
+    [Header("Botoes Geração")]
     public Button areaAberta;
     public Button areaFechada;
     public Button reuniaoAtiva;
     public Button reuniaoPassiva;
-    public GameObject parent;
+    
+
+    [Header("Cores")]
+    public Button cor0;
+    public Button cor1;
+    public Button cor2;
+    public Button cor3;
+
+
+    private GameManager gameManager;
     private InstantiationManager instantiationManager;
+    private ColorManager colorManager;
 
     private void Awake()
     {
+        gameManager = GetComponent<GameManager>();
         instantiationManager = GetComponent<InstantiationManager>();
+        colorManager = GetComponent<ColorManager>();
+
+
         areaAberta.onClick.AddListener( () => instantiationManager.Spawn( FormType.Cube, false ) );
         areaFechada.onClick.AddListener( () => instantiationManager.Spawn( FormType.Sphere, false ) );
         reuniaoAtiva.onClick.AddListener( () => instantiationManager.Spawn( FormType.Cube, true ) );
         reuniaoPassiva.onClick.AddListener( () => instantiationManager.Spawn( FormType.Sphere, true ) );
+
+        cor0.onClick.AddListener(() => colorManager.ChangeColor(0, gameManager.selectedObject));
+        cor1.onClick.AddListener(() => colorManager.ChangeColor(1, gameManager.selectedObject));
+        cor2.onClick.AddListener(() => colorManager.ChangeColor(2, gameManager.selectedObject));
+        cor3.onClick.AddListener(() => colorManager.ChangeColor(3, gameManager.selectedObject));
+
     }
 
     private void Task(FormType ft, bool isTransparent)
