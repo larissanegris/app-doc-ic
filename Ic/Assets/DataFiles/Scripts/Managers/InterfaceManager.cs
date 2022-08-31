26982327c32866b20b9ec277f71de97337f829ba
Lucca Paiva <luccafpaiva@gmail.com>
2022-08-31 20:44:01 -0300

    Mudanca da cor dos botoes
    Os botoes mudam de cor de acordo com o objeto selecionado

--- Ic/Assets/DataFiles/Scripts/Managers/InterfaceManager.cs
+++ Ic/Assets/DataFiles/Scripts/Managers/InterfaceManager.cs
@@ -24,13 +24,14 @@ public class InterfaceManager : MonoBehaviour
     private GameManager gameManager;
     private InstantiationManager instantiationManager;
     private ColorManager colorManager;
+    private SelectionManager selectionManager;
 
     private void Awake()
     {
         gameManager = GetComponent<GameManager>();
         instantiationManager = GetComponent<InstantiationManager>();
         colorManager = GetComponent<ColorManager>();
-
+        selectionManager = GetComponent<SelectionManager>();
 
         areaAberta.onClick.AddListener( () => instantiationManager.Spawn( FormType.Cube, false ) );
         areaFechada.onClick.AddListener( () => instantiationManager.Spawn( FormType.Sphere, false ) );
@@ -42,10 +43,20 @@ public class InterfaceManager : MonoBehaviour
         cor2.onClick.AddListener(() => colorManager.ChangeColor(2, gameManager.selectedObject));
         cor3.onClick.AddListener(() => colorManager.ChangeColor(3, gameManager.selectedObject));
 
+        selectionManager.selectionChange += ChangeBtnColor;
+
     }
 
     private void Task(FormType ft, bool isTransparent)
     {
         instantiationManager.Spawn( FormType.Cube, false );
+    }
+
+    private void ChangeBtnColor(GameObject tgt)
+    {
+        colorManager.ChangeBtnColor(0, cor0.gameObject, tgt.GetComponent<Form>().GetFormType());
+        colorManager.ChangeBtnColor(1, cor1.gameObject, tgt.GetComponent<Form>().GetFormType());
+        colorManager.ChangeBtnColor(2, cor2.gameObject, tgt.GetComponent<Form>().GetFormType());
+        colorManager.ChangeBtnColor(3, cor3.gameObject, tgt.GetComponent<Form>().GetFormType());
     }
 }
